175b30db037d0aacc1f00ad4c68c6a0381f83365
iAmOl3ks4ndr <o.oleksandr.m@gmail.com>
2025-05-08 05:09:30 +0100

    Updated vite.config.js for production release

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,15 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+                'resources/css/auth.css',
+                'resources/css/dashboard.css',
+                'resources/css/tasks.css',
                 'resources/js/app.js',
+                'resources/js/auth/login.jsx',
+                'resources/js/auth/signup.jsx',
+                'resources/js/dashboard/account.jsx',
+                'resources/js/dashboard/workspaces.jsx',
+                'resources/js/tasks.jsx',
             ],
             refresh: true,
         }),
